9bb34cb89b374786fda2fc8cae87a5aadd00a015
SutharHet <hetsuthar2001@gmail.com>
2022-01-04 18:00:55 +0530

    did some smal programs in milestone 3

--- python/app.py
+++ python/app.py
@@ -1,5 +1,32 @@
-string = input('Enter a string : ')
-rev_list = list(string)
-rev_list.reverse()
-rev_str = "".join(rev_list)
-print("reverse of '{}' is : {}".format(string,rev_str))
+def dec_to_bin():
+  while True:
+    try:
+      val = int(input('Enter Decimal Value : '))
+      print('{} to Binary is {}'.format(val,bin(val)))
+      break
+    except:
+      print('Invalid Input! Please Enter Decimal')
+  quit()
+
+def bin_to_dec():
+  while True:
+    try:
+      val = input('Enter Binary Value : ')
+      print('{} to Binary is {}'.format(val,int(val,2)))
+      break
+    except:
+      print('Invalid Input! Please Enter Binary')
+  quit()
+
+
+while True:
+  try:
+    choice = int(input('Choose optins(1/2) \n\t1. Binary to Decimal \t 2 .Decimal to Binary : '))
+    if choice in [1,2]:
+      if choice == 1:
+        bin_to_dec()
+      else:
+        dec_to_bin()
+    break
+  except:
+    print('Invalid Choice')
